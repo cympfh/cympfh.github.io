e0a04fc8358a6e6bb59c000f0134543c03c51b49
cympfh <cympfh@gmail.com>
2019-01-10 14:14:16 +0900

    lib/ Thu Jan 10 14:14:15 JST 2019

---
+++ lib/bit.count.rs
@@ -0,0 +1,16 @@
+fn main() {
+
+    {
+        let a = 3usize;
+        println!("{}", a.count_ones());
+    }
+    {
+        let a = 3u32;
+        println!("{}", a.count_ones());
+    }
+    {
+        let a = 3i32;
+        println!("{}", a.count_ones());
+    }
+
+}

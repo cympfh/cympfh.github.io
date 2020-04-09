13e2610c451024d00668451a1f2b1024464193b9
cympfh <cympfh@gmail.com>
2020-04-09 17:36:33 +0900

    Generalize procon/BIT

--- procon/seq.bit.rs
+++ procon/seq.bit.rs
@@ -1,29 +1,29 @@
 /// Sequence - Binary Indexed Tree (Fenwick Tree)
-use std::ops::Range;
-struct BIT { size: usize, array: Vec<i32> }
-impl BIT {
-    fn new(n: usize) -> BIT {
-        BIT  { size: n, array: vec![0; n + 1] }
+// @algebra.group.rs
+struct BIT<T> { size: usize, array: Vec<T> }
+impl<T: Group> BIT<T> {
+    fn new(n: usize) -> BIT<T> {
+        BIT  { size: n, array: vec![T::zero(); n + 1] }
     }
-    fn add(&mut self, idx: usize, w: i32) {
+    fn add(&mut self, idx: usize, w: T) {
         let mut x = idx + 1;
         while x <= self.size {
-            self.array[x] += w;
+            self.array[x] = self.array[x] + w;
             let xi = x as i32;
             x += (xi&-xi) as usize;
         }
     }
-    fn sum_up(&self, idx: usize) -> i32 { // [0, idx)
-        let mut sum = 0;
+    fn sum_up(&self, idx: usize) -> T { // [0, idx)
+        let mut sum = T::zero();
         let mut x = idx;
         while x > 0 {
-            sum += self.array[x];
+            sum = sum + self.array[x];
             let xi = x as i32;
-            x -= (xi&-xi) as usize;
+            x -= (xi& - xi) as usize;
         }
         sum
     }
-    fn sum(&self, range: Range<usize>) -> i32 {
+    fn sum(&self, range: std::ops::Range<usize>) -> T {
         self.sum_up(range.end) - self.sum_up(range.start)
     }
 }

6fabde7ca310b7c15d80687ea76a0b86d50ae605
/c/ympfh <cympfh@gmail.com>
2020-05-16 18:42:27 +0900

    Generalize CumSum (#7)

--- procon/seq.cumsum2d.rs
+++ procon/seq.cumsum2d.rs
@@ -1,10 +1,11 @@
 /// Sequence - 2D Cumulative Summation
-struct CumSum2D(Vec<Vec<i64>>);
-impl CumSum2D {
-    fn new(data: &Vec<Vec<i64>>) -> CumSum2D {
+// @algebra.group.rs
+struct CumSum2D<T>(Vec<Vec<T>>);
+impl<T: Group> CumSum2D<T> {
+    fn new(data: &Vec<Vec<T>>) -> CumSum2D<T> {
         let h = data.len();
         let w = data[0].len();
-        let mut cs = vec![vec![0; w + 1]; h + 1];
+        let mut cs = vec![vec![T::zero(); w + 1]; h + 1];
         for i in 0..h {
             for j in 0..w {
                 cs[i + 1][j + 1] = data[i][j] + cs[i][j + 1] + cs[i + 1][j] - cs[i][j];
@@ -12,14 +13,14 @@ impl CumSum2D {
         }
         CumSum2D(cs)
     }
-    fn sum_up(&self, x: usize, y: usize) -> i64 {
+    fn sum_up(&self, x: usize, y: usize) -> T {
         let x = std::cmp::min(x, self.0.len());
         let y = std::cmp::min(y, self.0[0].len());
         self.0[x][y]
     }
-    fn sum(&self, xrange: std::ops::Range<usize>, yrange: std::ops::Range<usize>) -> i64 {
+    fn sum(&self, xrange: std::ops::Range<usize>, yrange: std::ops::Range<usize>) -> T {
         if xrange.end <= xrange.start || yrange.end <= yrange.start {
-            0
+            T::zero()
         } else {
             self.sum_up(xrange.end, yrange.end)
                 - self.sum_up(xrange.start, yrange.end)

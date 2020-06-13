4448ec6a53de7d4640dfded280db0efe2849519d
cympfh <cympfh@gmail.com>
2020-06-14 01:09:05 +0900

    rust 2018 && clippy

--- procon/collections.defaultmap.rs
+++ procon/collections.defaultmap.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, Clone)]
 struct DefaultMap<S, T> {
     data: std::collections::HashMap<S, T>,
     default: T,
@@ -6,12 +7,18 @@ impl<S: Eq + std::hash::Hash, T> DefaultMap<S, T> {
     fn new(default: T) -> DefaultMap<S, T> {
         DefaultMap {
             data: std::collections::HashMap::new(),
-            default: default
+            default,
         }
     }
-    fn keys(&self) -> std::collections::hash_map::Keys<S, T> { self.data.keys() }
-    fn iter(&self) -> std::collections::hash_map::Iter<S, T> { self.data.iter() }
-    fn len(&self) -> usize { self.data.len() }
+    fn keys(&self) -> std::collections::hash_map::Keys<S, T> {
+        self.data.keys()
+    }
+    fn iter(&self) -> std::collections::hash_map::Iter<S, T> {
+        self.data.iter()
+    }
+    fn len(&self) -> usize {
+        self.data.len()
+    }
 }
 impl<S: Eq + std::hash::Hash, T> std::ops::Index<S> for DefaultMap<S, T> {
     type Output = T;
@@ -25,7 +32,8 @@ impl<S: Eq + std::hash::Hash, T> std::ops::Index<S> for DefaultMap<S, T> {
 }
 impl<S: Eq + std::hash::Hash + Clone, T: Clone> std::ops::IndexMut<S> for DefaultMap<S, T> {
     fn index_mut(&mut self, key: S) -> &mut Self::Output {
-        self.data.entry(key.clone()).or_insert(self.default.clone());
+        let val = self.default.clone();
+        self.data.entry(key.clone()).or_insert(val);
         self.data.get_mut(&key).unwrap()
     }
 }

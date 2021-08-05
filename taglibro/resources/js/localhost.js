f7f19d7a56dc30514d31c00960e4b113af4b0062
cympfh <cympfh@gmail.com>
2021-08-05 14:37:17 +0900

    taglibro/ Thu Aug  5 14:37:17 JST 2021

--- taglibro/resources/js/localhost.js
+++ taglibro/resources/js/localhost.js
@@ -1,10 +1,21 @@
-// Append `.html` to links
-document.addEventListener('DOMContentLoaded', () => {
-  let n = document.links.length;
-  for (var i = 0; i < n; ++i) {
-    let href = document.links[i].href;
-    if (href.indexOf('localhost') >= 0 && href.indexOf('taglibro') >= 0 && href.indexOf('.html') == -1) {
-      document.links[i].href = `${href}.html`;
+/*
+ * Append `.html` to links
+ */
+
+function isLocal(url) {
+  return url.indexOf("localhost") >= 0 || url.indexOf("127.0.0.1") >= 0;
+}
+
+function isTaglibro(url) {
+  return url.indexOf("taglibro") >= 0 || url.indexOf(".html") == -1;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const n = document.links.length;
+  for (let i = 0; i < n; ++i) {
+    let url = document.links[i].href;
+    if (isLocal(url) && isTaglibro(url)) {
+      document.links[i].href = `${url}.html`;
     }
   }
 });

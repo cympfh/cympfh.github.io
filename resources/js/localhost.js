d4c4e3710acb409ec7f6eca20a9416aea961d15e
cympfh <cympfh@gmail.com>
2021-12-01 01:56:06 +0900

    aiura/ 2021年 12月  1日 水曜日 01:56:06 JST

--- resources/js/localhost.js
+++ resources/js/localhost.js
@@ -3,7 +3,7 @@
  */
 
 function isLocalPage(url) {
-  return ( url.indexOf("localhost") >= 0 || url.indexOf("127.0.0.1") >= 0 ) && url.indexOf("#") == -1;
+  return ( url.indexOf("localhost") >= 0 || url.indexOf("127.0.0.1") >= 0 || url.indexOf("192.168.") >= 0 ) && url.indexOf("#") == -1;
 }
 
 document.addEventListener("DOMContentLoaded", () => {

ed528b90144f1f512aa8ef0e7d170fcde2064743
cympfh <cympfh@gmail.com>
2022-05-06 16:38:02 +0900

    taglibro/2022/04

--- taglibro/script/edit.sh
+++ taglibro/script/edit.sh
@@ -34,5 +34,4 @@ if [ ! -f "${TOKEN}.md" ]; then
     ( echo "% $(datetitle "$TOKEN")"; echo ) > "${TOKEN}.md"
 fi
 
-vim "$TOKEN.md"
-# vim -c "au BufWritePost *.md :silent !make $TOKEN.html >/dev/null 2>&1" "$TOKEN.md"
+$EDITOR "$TOKEN.md"
